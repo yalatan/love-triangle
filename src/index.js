1eb3b45914f3306fe66d2d74ed261742fb5c225f
yalatan <nikitinanf@gmail.com>
2019-02-21 23:25:59 +0300

    Love triangle
    Love triangle done

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+var count = 0;
+var para;
+  
+ for (let a = 0; a < preferences.length; a++){
+     var n = a + 1; 
+    var k1 = preferences[n-1]; 
+    var k2 = preferences[k1-1]; 
+    var k3 = preferences[k2 - 1]; 
+    if(n == k3 && n != k1){
+        count++; 
+    } else{ count += 0;}
+ };
+   para = count / 3;
+   return para;
+};  
+
+
